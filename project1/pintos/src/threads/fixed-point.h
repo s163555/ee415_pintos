cfe820890cfc84546fc0db829046c27f19eae835
unknown <s163555@student.dtu.dk>
2022-03-23 21:05:57 +0900

    Initial commit

---
+++ project1/pintos/src/threads/fixed-point.h
@@ -0,0 +1,20 @@
+#ifndef THREAD_FIXED_POINT_H
+#define THREAD_FIXED_POINT_H
+
+#define F (1 << 14)
+#define INT_MAX ((1 << 31) - 1)
+#define INT_MIN (-(1 << 31))
+
+int int_to_fp (int n);
+int fp_to_int (int x);
+int fp_to_int_round (int x);
+int add_fp (int x, int y);
+int sub_fp (int x, int y);
+int add_mixed (int x, int n);
+int sub_mixed (int x, int n);
+int mult_fp (int x, int y);
+int mult_mixed (int x, int n);
+int div_fp (int x, int y);
+int div_mixed (int x, int n);
+
+#endif /* threads/fixed-point.h */
